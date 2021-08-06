a22b3cb7989d9dc5a5b5b1232e70ae0d13df6115
zerk <zerkvii@gmail.com>
2021-08-06 17:13:47 +0800

    add:zsh_conf.sh

---
+++ install_zsh.sh
@@ -0,0 +1,9 @@
+sudo apt install zsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+sed -i -- 's/plugins=(git)/plugins=(zsh-syntax-highlighting git zsh-autosuggestions )/g' ~/.zshrc
+sed -i -- 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g' ~/.zshrc
+chsh -s /bin/zsh
