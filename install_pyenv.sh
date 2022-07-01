abbad7bf9e233326405361e845bbd98fcd032e93
zerkvii <zerkvii@gmail.com>
2022-07-01 17:08:57 +0800

    cleanup:

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -1,6 +1,7 @@
 curl https://pyenv.run | 
 
-
+cp -r pyenv-virtualenv ~/.pyenv/plugins/
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
